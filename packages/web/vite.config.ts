7ec95aef64a7800435bdcb2a914da061e2a214ff
Alex Astafyev <62753371+alex-astafyev@users.noreply.github.com>
2026-03-01 17:45:16 +0400

    fixed service worker config

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -42,6 +42,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2}'],
+        navigateFallbackDenylist: [/^\/admin/, /^\/cable/],
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
